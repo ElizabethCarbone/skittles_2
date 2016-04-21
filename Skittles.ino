2d0217a9e963f6cf69a6d559dae266b147e88422
ElizabethCarbone <ejcarb02@louisville.edu>
2016-04-21 09:58:02 -0400

    Added files via upload

---
+++ Skittles.ino
@@ -0,0 +1,118 @@
+#include "Servo.h"
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Servo feederServo;
+Servo flickerServo;
+Servo sortServo;
+Servo stayServo;
+
+#define redpin 3
+#define greenpin 5
+#define bluepin 6
+
+#define commonAnode false
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+byte gammatable[256];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+ 
+  flickerServo.attach (8);
+  flickerServo.write (35);
+   stayServo.attach(10);
+   stayServo.write (90);
+  //sortServo.attach (4);
+
+  Serial.println("Color View Test!");
+
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1); // halt!
+  }
+  
+  // use these three pins to drive an LED
+  pinMode(redpin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+  
+  // thanks PhilB for this gamma table!
+  // it helps convert RGB colors to what humans see
+  for (int i=0; i<256; i++) {
+    float x = i;
+    x /= 255;
+    x = pow(x, 2.5);
+    x *= 255;
+      
+    if (commonAnode) {
+      gammatable[i] = 255 - x;
+    } else {
+      gammatable[i] = x;      
+    }
+    //Serial.println(gammatable[i]);
+  }
+}
+  
+
+  
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ feederServo.attach(9); 
+ feederServo.write (90);
+ delay (5000);
+ 
+ feederServo.detach(); //stop feeding skittles
+
+ stayServo.write (90); // center to block skittle
+ delay (2000);
+
+ flickerServo.write (85); //move skittle to sensor
+ delay (2000);
+
+  
+ uint16_t clear, red, green, blue;
+
+  tcs.setInterrupt(false);      // turn on LED
+
+  delay(60);  // takes 50ms to read 
+  
+  tcs.getRawData(&red, &green, &blue, &clear);
+
+  tcs.setInterrupt(true);  // turn off LED
+  
+  Serial.print("C:\t"); Serial.print(clear);
+  Serial.print("\tR:\t"); Serial.print(red);
+  Serial.print("\tG:\t"); Serial.print(green);
+  Serial.print("\tB:\t"); Serial.print(blue);
+
+  // Figure out some basic hex code for visualization
+  uint32_t sum = clear;
+  float r, g, b;
+  r = red; r /= sum;
+  g = green; g /= sum;
+  b = blue; b /= sum;
+  r *= 256; g *= 256; b *= 256;
+  Serial.print("\t");
+  Serial.print((int)r, HEX); Serial.print((int)g, HEX); Serial.print((int)b, HEX);
+  Serial.println();
+
+  //Serial.print((int)r ); Serial.print(" "); Serial.print((int)g);Serial.print(" ");  Serial.println((int)b );
+
+  analogWrite(redpin, gammatable[(int)r]);
+  analogWrite(greenpin, gammatable[(int)g]);
+  analogWrite(bluepin, gammatable[(int)b]);
+ 
+ delay (3000);
+  stayServo.write (180); //send skittle right
+ delay (2000);
+ flickerServo.write(35); //flick skittle off sensor
+ delay (2000);
+ stayServo.write (0); //send skittle down shoot to left 
+ delay (2000);
+}
